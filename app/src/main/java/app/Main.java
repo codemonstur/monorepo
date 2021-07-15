72f194b0041b8365ca1c9f03bb6ad22564b9f7f5
Jurgen Voorneveld <jegvoorneveld@gmail.com>
2021-07-15 14:55:45 +0200

    Initial commit

---
+++ app/src/main/java/app/Main.java
@@ -0,0 +1,11 @@
+package app;
+
+import static library.SomeClass.HELLO;
+
+public enum Main {;
+
+    public static void main(final String... args) {
+        System.out.println(HELLO);
+    }
+
+}
---
+++ library/src/main/java/library/SomeClass.java
@@ -0,0 +1,7 @@
+package library;
+
+public final class SomeClass {
+
+    public static final String HELLO = "Hello, world!";
+
+}
